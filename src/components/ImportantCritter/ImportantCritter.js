024978c59d6b743ffe2808c46dc1daa5c1e99280
Rosemary Chen <rosemary.chen@clover.com>
2020-04-28 21:14:08 -0700

    read queryParams to set initial state

--- src/components/ImportantCritter/ImportantCritter.js
+++ src/components/ImportantCritter/ImportantCritter.js
@@ -9,17 +9,18 @@ import React from "react";
 import { CritterTable } from "../CritterTable/CritterTable";
 import { fishWithDates } from "../../data/FishData";
 import { insectWithDates } from "../../data/InsectData";
-import { getCrittersAvailableThisMonth, getCrittersLeavingThisMonth } from "../../data/utils";
+import { getCrittersAvailableByMonth, getCrittersLeavingByMonth, getQueryParam, monthNameToNumMap } from "../../data/utils";
 
 export const ImportantCritterSection = ({ critter }) => {
   const critterWithDates = critter === 'fish' ? fishWithDates : insectWithDates;
+  const [monthName, monthNum] = getMonth();
 
   return (
     <>
       <div style={{margin: '20px 0px'}}>
         <CritterTable
-          title="I'm leaving this month :("
-          critterData={getCrittersLeavingThisMonth(critterWithDates)}
+          title={`I'm leaving end of ${monthName} D:`}
+          critterData={getCrittersLeavingByMonth(critterWithDates, monthName)}
           isImportantSection={true}
           critter={critter}
         />
@@ -27,12 +28,25 @@ export const ImportantCritterSection = ({ critter }) => {
 
       <div style={{margin: '20px 0px'}}>
         <CritterTable
-          title="I'm available! Catch me if you can! >:)"
-          critterData={getCrittersAvailableThisMonth(critterWithDates)}
+          title={`I'm available this ${monthName}! Catch me if you can! >:)`}
+          critterData={getCrittersAvailableByMonth(critterWithDates, monthNum)}
           isImportantSection={true}
           critter={critter}
         />
       </div>
     </>
   );
+};
+
+/**
+ * Returns the current month or the month specified in queryParams
+ */
+const getMonth = () => {
+  const queryMonth = getQueryParam().month;
+  const isQueryMonthInvalid = isNaN(monthNameToNumMap[queryMonth]);
+  const date = new Date();
+  const monthNum = isQueryMonthInvalid ? date.getMonth() : monthNameToNumMap[queryMonth];
+  const monthName = isQueryMonthInvalid ? date.toLocaleDateString("default", { month: "long" }) : queryMonth;
+
+  return [monthName, monthNum];
 };
